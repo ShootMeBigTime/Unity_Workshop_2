5b29c414f12e0bc5c4856f15381f58bea245ed0b
ShootMeBigTime <70698888+ShootMeBigTime@users.noreply.github.com>
2022-09-22 11:09:35 +0200

    Player movement + pickup script

---
+++ Workshop_3/Assets/Scripts/PickUpScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour
+{
+    public GameObject givenItem;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals("Player"))
+        {
+            Debug.Log("Test");
+            other.GetComponent<Backpack>().items.Add(givenItem);
+            Destroy(gameObject);
+        }
+    }
+}
